99f9e32c1320001abef5caec071112cf5f22b9d3
FedericoV <vaglicafederico95@gmail.com>
2020-09-09 16:29:15 +0200

    Put request to improve

---
+++ script.js
@@ -0,0 +1,115 @@
+$(document).ready(function(){
+
+    $(document).on('click','.add-btn', function(){
+        $('.main').html('')
+        var newElement = $('.input-add').val();
+
+        if(newElement.length != 0){
+            $('.list').html('')
+            createElement(newElement)
+            readList();         
+        }  
+           
+    })
+
+    $(document).on('click','.delete', function (){
+        $('.main').html('')
+        var elementDelete = $(this)
+        var id = elementDelete.parent().attr('data-id');
+        deleteElementList(id);
+    })
+
+    $(document).on('click','.update', function (){
+        $('.main').html('')
+        var id = $(this).attr('data-id');
+        var text = $('.input [data-id="' + id + '"]').val();
+        console.log(id);
+        console.log(text)
+
+        upDateElement(id,text)
+    })
+
+    
+    
+    
+    function readList(){
+        $.ajax(
+          {
+            url: "http://157.230.17.132:3033/todos",
+            method: "GET",
+            success: function (data)
+            {
+              var source = $("#entry-template").html();
+              var template = Handlebars.compile(source);
+              for (var i = 0; i < data.length; i++) {
+                var todos = data[i];
+                var context = {
+                  text: todos.text,
+                  id: todos.id
+                };
+                var html = template(context);
+                $('.main').append(html);
+              }
+      
+            },
+            error: function () {
+              alert("E' avvenuto un errore. ");
+            }
+          });
+      
+      }
+
+
+
+    function createElement(todoValue) {
+    $.ajax(
+        {
+        url: "http://157.230.17.132:3033/todos",
+        method: "POST",
+        data:{
+            text: todoValue
+        },
+        success: function (){
+            $('.list').html()
+        },
+        error: function () {
+            alert("E' avvenuto un errore. ");
+        }
+        });
+    }
+
+    function deleteElementList(id){
+        $.ajax(
+          {
+            url: "http://157.230.17.132:3033/todos/" + id,
+            method: "DELETE",
+            success: function (){
+                readList()
+            },
+            error: function () {
+              alert("E' avvenuto un errore. ");
+            }
+          });
+      }
+
+    function upDateElement(id,toUpdate){
+        $.ajax(
+            {
+              url: "http://157.230.17.132:3033/todos/" + id,
+              method: "PUT",
+              data: {
+                  text: toUpdate
+              },
+
+              success: function (){
+
+                  readList()
+              },
+              error: function () {
+                alert("E' avvenuto un errore. ");
+              }
+            });
+    }
+
+
+})
